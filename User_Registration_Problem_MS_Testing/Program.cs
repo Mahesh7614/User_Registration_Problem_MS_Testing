8e3997f14a73ccc54a5cc3f31af9be9e0842228b
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-23 18:41:36 +0530

    Validate Sample Emails

--- User_Registration_Problem_MS_Testing/Program.cs
+++ User_Registration_Problem_MS_Testing/Program.cs
@@ -5,25 +5,25 @@
         static void Main(string[] args)
         {
             User_Registration user = new User_Registration();
-            //Console.Write("Enter First Name : ");
-            //string firstName = Console.ReadLine();
-            //Console.WriteLine(user.FirstNameAndLastName(firstName));
+            Console.Write("Enter First Name : ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine(user.FirstNameAndLastName(firstName));
 
-            //Console.Write("Enter Last Name : ");
-            //string LastName = Console.ReadLine();
-            //Console.WriteLine(user.FirstNameAndLastName(LastName));
+            Console.Write("Enter Last Name : ");
+            string LastName = Console.ReadLine();
+            Console.WriteLine(user.FirstNameAndLastName(LastName));
 
-            //Console.Write("Enter Email ID : ");
-            //string email = Console.ReadLine();
-            //Console.WriteLine(user.Email(email));
+            Console.Write("Enter Email ID : ");
+            string email = Console.ReadLine();
+            Console.WriteLine(user.Email(email));
 
-            //Console.Write("Enter Mobile Number with Country Code and Space between them : ");
-            //string mobileNumber = Console.ReadLine();
-            //Console.WriteLine(user.MobileNumber(mobileNumber));
+            Console.Write("Enter Mobile Number with Country Code and Space between them : ");
+            string mobileNumber = Console.ReadLine();
+            Console.WriteLine(user.MobileNumber(mobileNumber));
 
-            //Console.WriteLine("Enter Password min 8 Characters : ");
-            //string password = Console.ReadLine();
-            //Console.WriteLine(user.PassWord(password));
+            Console.WriteLine("Enter Password min 8 Characters : ");
+            string password = Console.ReadLine();
+            Console.WriteLine(user.PassWord(password));
 
             Console.Write("Enter Email Id Given in Chart : ");
             string emailID = Console.ReadLine();
