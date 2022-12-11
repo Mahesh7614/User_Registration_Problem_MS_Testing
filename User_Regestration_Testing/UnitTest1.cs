a60421d20ee96c3dd305b639685da657c5f1f159
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-12-11 22:15:05 +0530

    TestCasesForSampleEmails

--- User_Regestration_Testing/UnitTest1.cs
+++ User_Regestration_Testing/UnitTest1.cs
@@ -87,12 +87,35 @@ namespace User_Regestration_Testing
             Assert.AreEqual(expected,actual);
         }
         [TestMethod]
-        public void TestSampleEmails()
+        [DataRow("abc@yahoo.com,", true)]
+        [DataRow("abc-100@yahoo.com,", true)]
+        [DataRow("abc.100@yahoo.com", true)]
+        [DataRow("abc111@abc.com,", true)]
+        [DataRow("abc-100@abc.net,", true)]
+        [DataRow("abc.100@abc.com.au", true)]
+        [DataRow("abc@1.com,", true)]
+        [DataRow("abc@gmail.com.com", true)]
+        [DataRow("abc+100@gmail.com", true)]
+        [DataRow("abc", false)]
+        [DataRow("abc@.com.my", false)]
+        [DataRow("abc123@gmail.a", false)]
+        [DataRow("abc123@.com", false)]
+        [DataRow("abc123@.com.com", false)]
+        [DataRow(".abc@abc.com", false)]
+        [DataRow("abc()*@gmail.com", false)]
+        [DataRow("abc..2002@gmail.com", false)]
+        [DataRow("abc.@gmail.com", false)]
+        [DataRow("abc@abc@gmail.com", false)]
+        [DataRow("abc@gmail.com.1a", false)]
+        [DataRow("abc@gmail.com.aa.au", false)]
+        [DataRow("abc@%*.com", false)]
+
+        public void TestSampleEmails(string email, bool expected)
         {
             User_Registration user = new User_Registration();
-            bool result = user.SampleEmail("abc.100@abc.com.au");
+            bool actual = user.SampleEmail(email);
 
-            Assert.IsTrue(result);
+            Assert.AreEqual(expected,actual);
         }
 
     }
